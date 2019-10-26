a57a3c4e16fd6ec0bcf3cc96f59f64c508117c92
Roman Marakulin <marakulin@joom.com>
2019-10-26 09:01:35 +0300

    add shopping centers grabber

---
+++ offlineUpdater/venues_static.py
@@ -0,0 +1,9 @@
+from here_sdk import Point, BoundingBox
+
+VENUE_BB = {
+    'ORYAD': BoundingBox(Point(55.756858, 37.613899), Point(55.751663, 37.627889))
+}
+
+VENUE_IDS = {
+    'ORYAD': 'DM_20518'
+}
