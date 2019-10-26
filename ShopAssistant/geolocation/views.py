1f0f96b8e74a9b49fdd65b78a85022b5e0b02147
Cornul11 <cornul11@gmail.com>
2019-10-26 06:46:27 +0300

    location fix

--- ShopAssistant/geolocation/views.py
+++ ShopAssistant/geolocation/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
-
+@csrf_exempt
 def index(request):
-    lon = request.POST['lon']
-    lat = request.POST['lat']
-    return HttpResponse(lon)
+    path = request.get_full_path()
+    print(path)
+    path = path.replace('/geostatus/post/?', '')
+    path = path.split('&')
+    lon = float(path[0].replace('lon=', ''))
+    lat = float(path[1].replace('lat=', ''))
+    return HttpResponse([lon, ' ', lat])
 # Create your views here.
